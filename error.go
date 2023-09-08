572dcde2c05b20cb931d974c88a44a18776452ad
pewpewnor <newpatchnotworking2@tutanota.com>
2023-09-09 00:21:17 +0700

    feat: add json response generator

---
+++ error.go
@@ -0,0 +1,58 @@
+package auxi
+
+import (
+	"strconv"
+)
+
+type ErrorResponseData struct {
+	ErrorData errorResponseContent `json:"error"`
+}
+
+func (e *ErrorResponseData) AddValidation(validation ErrorResponseValidation) {
+	e.ErrorData.ValidationErrors = append(e.ErrorData.ValidationErrors, validation)
+}
+
+func (e ErrorResponseData) Error() string {
+	return e.ErrorData.Message
+}
+
+type errorResponseContent struct {
+	Code             string                    `json:"code"`
+	Message          string                    `json:"message"`
+	Details          string                    `json:"details"`
+	ValidationErrors []ErrorResponseValidation `json:"validationErrors"`
+}
+
+type ErrorResponseValidation struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func SimpleErrorResponse(message string, details string) ErrorResponseData {
+	return ErrorResponseData{
+		ErrorData: errorResponseContent{
+			Message: message,
+			Details: details,
+		},
+	}
+}
+
+func SimpleErrorResponseFromError(message string, err error) ErrorResponseData {
+	return ErrorResponseData{
+		ErrorData: errorResponseContent{
+			Message: message,
+			Details: err.Error(),
+		},
+	}
+}
+
+func ErrorResponse(code int, message string, details string, validationErrors []ErrorResponseValidation) ErrorResponseData {
+	return ErrorResponseData{
+		ErrorData: errorResponseContent{
+			strconv.Itoa(code),
+			message,
+			details,
+			validationErrors,
+		},
+	}
+}
---
+++ success.go
@@ -0,0 +1,22 @@
+package auxi
+
+type SuccessResponseData struct {
+	Status  string                 `json:"status"`
+	Data    map[string]interface{} `json:"data"`
+	Message string                 `json:"message"`
+}
+
+func SimpleSuccessResponse(message string) SuccessResponseData {
+	return SuccessResponseData{
+		Status:  "success",
+		Message: message,
+	}
+}
+
+func SuccessResponse(data map[string]interface{}, message string) SuccessResponseData {
+	return SuccessResponseData{
+		Status:  "success",
+		Message: message,
+		Data:    data,
+	}
+}
