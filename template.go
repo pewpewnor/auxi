345418bc68b31d544ee0831ffaf24f81138e992e
pewpewnor <newpatchnotworking2@tutanota.com>
2023-09-09 22:51:28 +0700

    feat: add template code

---
+++ template.go
@@ -0,0 +1,61 @@
+package auxi
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type AuthorizationHeader struct{}
+
+func (ah AuthorizationHeader) GetBearerToken(r *http.Request, tokenPrefix string) (string, error) {
+	value := r.Header.Get("Authorization")
+	if value == "" {
+		err := Respond.SimpleError("No authorization header or value given")
+		return "", err
+	}
+
+	values := strings.Split(value, " ")
+	if len(values) != 2 {
+		err := Respond.SimpleError("Authorization header value is malformed")
+		err.AddValidation(Respond.CreateValidation(
+			"Authorization header",
+			"Expected exactly 2 values separated by spaces",
+		))
+
+		return "", err
+	}
+	if values[0] != tokenPrefix {
+		err := Respond.SimpleError("Authorization header value is malformed")
+		err.AddValidation(Respond.CreateValidation(
+			"Authorization header",
+			fmt.Sprintf("First value (token prefix) must be '%v'", tokenPrefix),
+		))
+
+		return "", err
+	}
+
+	return values[1], nil
+}
+
+type Middleware func(next http.HandlerFunc) http.HandlerFunc
+
+func NewCORSMiddleware(options map[string]string) Middleware {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			r.Header.Set("Access-Control-Allow-Origin", "*")
+			r.Header.Set("Access-Control-Allow-Credentials", "true")
+			r.Header.Set(
+				"Access-Control-Allow-Headers",
+				"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			r.Header.Set(
+				"Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+			for k, v := range options {
+				r.Header.Set(k, v)
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
