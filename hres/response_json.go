80e6257dbfa629d9ec02c5127c82e35b4a0fab5a
pewpewnor <newpatchnotworking2@tutanota.com>
2023-09-09 20:25:00 +0700

    feat: use error json response

--- hres/response_json.go
+++ hres/response_json.go
@@ -2,18 +2,29 @@ package hres
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func RespondWithJSON(w http.ResponseWriter, data any, httpStatusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+
 	response, err := json.Marshal(data)
 	if err != nil {
-		http.Error(w, "Server could not encode response", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+
+		defer panic(fmt.Sprintf("Could not marshal response %v to JSON: %v", response, err))
+
+		errorResponse, err := json.Marshal(SimpleErrorFromErr("Could not marshal response to JSON", err))
+		if err != nil {
+			w.Write([]byte("Could not marshal response to JSON"))
+		}
+
+		w.Write(errorResponse)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(httpStatusCode)
 	w.Write(response)
 }
