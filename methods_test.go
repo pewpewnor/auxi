9ada0a81261aee58b696d1679b03a6ff8b811edd
pewpewnor <newpatchnotworking2@tutanota.com>
2023-09-09 14:07:18 +0700

    refactor: collapse function

--- methods_test.go
+++ methods_test.go
@@ -22,37 +22,6 @@ var expectedTestPerson = testPerson{
 	Age:  69,
 }
 
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	var params testPerson
-
-	params.Name = r.URL.Query().Get("name")
-	if params.Name == "" {
-		http.Error(w, "query string missing field 'name'", http.StatusBadRequest)
-		return
-	}
-
-	age, err := strconv.Atoi(r.URL.Query().Get("age"))
-	if err != nil {
-		http.Error(w, "query string missing field 'age' or field 'age' is not an integer", http.StatusBadRequest)
-		return
-	}
-	params.Age = age
-
-	res.RespondWithJSON(w, params, http.StatusOK)
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	var person testPerson
-
-	err := json.NewDecoder(r.Body).Decode(&person)
-	if err != nil {
-		http.Error(w, "body malformed", http.StatusBadRequest)
-		return
-	}
-
-	res.RespondWithJSON(w, person, http.StatusOK)
-}
-
 func testGetHandler(server *httptest.Server, t *testing.T) {
 	resp, err := http.Get(server.URL + "/test" + fmt.Sprintf("?name=%v&age=%v", expectedTestPerson.Name, expectedTestPerson.Age))
 	if err != nil {
@@ -104,8 +73,35 @@ func TestHandleMethods(t *testing.T) {
 	mux := NewServeMux()
 
 	mux.HandleMethods("/test", MethodHandlers{
-		GET:  getHandler,
-		POST: postHandler,
+		GET: func(w http.ResponseWriter, r *http.Request) {
+			var params testPerson
+
+			params.Name = r.URL.Query().Get("name")
+			if params.Name == "" {
+				http.Error(w, "query string missing field 'name'", http.StatusBadRequest)
+				return
+			}
+
+			age, err := strconv.Atoi(r.URL.Query().Get("age"))
+			if err != nil {
+				http.Error(w, "query string missing field 'age' or field 'age' is not an integer", http.StatusBadRequest)
+				return
+			}
+			params.Age = age
+
+			res.RespondWithJSON(w, params, http.StatusOK)
+		},
+		POST: func(w http.ResponseWriter, r *http.Request) {
+			var person testPerson
+
+			err := json.NewDecoder(r.Body).Decode(&person)
+			if err != nil {
+				http.Error(w, "body malformed", http.StatusBadRequest)
+				return
+			}
+
+			res.RespondWithJSON(w, person, http.StatusOK)
+		},
 	})
 
 	server := httptest.NewServer(mux)
