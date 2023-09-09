3ec9c2f27579c8af27aad352b3ab3617bef866b7
pewpewnor <newpatchnotworking2@tutanota.com>
2023-09-09 14:03:29 +0700

    feat: add response with json

---
+++ res/response_json.go
@@ -0,0 +1,19 @@
+package res
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func RespondWithJSON(w http.ResponseWriter, data any, httpStatusCode int) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Server could not encode response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(httpStatusCode)
+	w.Write(response)
+}
