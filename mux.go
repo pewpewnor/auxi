2eb8d95d5c106b75ee8274e721da2328e42b620b
pewpewnor <newpatchnotworking2@tutanota.com>
2023-09-18 21:47:08 +0700

    feat: add use feature

--- mux.go
+++ mux.go
@@ -1,6 +1,8 @@
 package auxi
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Middleware func(next http.HandlerFunc) http.HandlerFunc
 
@@ -12,6 +14,55 @@ type MethodHandlers struct {
 	PATCH   func(http.ResponseWriter, *http.Request)
 	HEAD    func(http.ResponseWriter, *http.Request)
 	OPTIONS func(http.ResponseWriter, *http.Request)
+}
+
+type ServeMux struct {
+	*http.ServeMux
+	middlewareChain *MiddlewareChain
+}
+
+func NewServeMux() *ServeMux {
+	return &ServeMux{
+		http.NewServeMux(),
+		NewMiddlewareChain(),
+	}
+}
+
+func (mux *ServeMux) Use(middleware Middleware) {
+	mux.middlewareChain.AddMiddleware(middleware)
+}
+
+func (mux *ServeMux) UseChain(middlewareChain *MiddlewareChain) {
+	mux.middlewareChain.ApplyToChain(middlewareChain)
+}
+
+func (mux *ServeMux) HandleMethods(pattern string, methodHandlers MethodHandlers) {
+	mux.HandleFunc(pattern, http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case http.MethodGet:
+				callMethodHandler(w, r, methodHandlers.GET)
+			case http.MethodPost:
+				callMethodHandler(w, r, methodHandlers.POST)
+			case http.MethodPut:
+				callMethodHandler(w, r, methodHandlers.PUT)
+			case http.MethodDelete:
+				callMethodHandler(w, r, methodHandlers.DELETE)
+			case http.MethodPatch:
+				callMethodHandler(w, r, methodHandlers.PATCH)
+			case http.MethodHead:
+				callMethodHandler(w, r, methodHandlers.HEAD)
+			case http.MethodOptions:
+				callMethodHandler(w, r, methodHandlers.OPTIONS)
+			default:
+				Respond.JSON(w, Respond.SError("Method not supported"),
+					http.StatusMethodNotAllowed)
+			}
+		}))
+}
+
+func (mux *ServeMux) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	mux.ServeMux.HandleFunc(pattern, mux.middlewareChain.Apply(handler))
 }
 
 type MiddlewareChain struct {
@@ -38,45 +89,10 @@ func (c *MiddlewareChain) Apply(handler http.HandlerFunc) http.HandlerFunc {
 	return resultHandler
 }
 
-func (c *MiddlewareChain) ApplyToChain(chain MiddlewareChain) *MiddlewareChain {
+func (c *MiddlewareChain) ApplyToChain(chain *MiddlewareChain) *MiddlewareChain {
 	return &MiddlewareChain{
 		middlewares: append(c.middlewares, chain.middlewares...),
 	}
-}
-
-type ServeMux struct {
-	*http.ServeMux
-}
-
-func NewServeMux() *ServeMux {
-	return &ServeMux{
-		http.NewServeMux(),
-	}
-}
-
-func (mux *ServeMux) HandleMethods(pattern string, methodHandlers MethodHandlers) {
-	mux.HandleFunc(pattern, http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			switch r.Method {
-			case http.MethodGet:
-				callMethodHandler(w, r, methodHandlers.GET)
-			case http.MethodPost:
-				callMethodHandler(w, r, methodHandlers.POST)
-			case http.MethodPut:
-				callMethodHandler(w, r, methodHandlers.PUT)
-			case http.MethodDelete:
-				callMethodHandler(w, r, methodHandlers.DELETE)
-			case http.MethodPatch:
-				callMethodHandler(w, r, methodHandlers.PATCH)
-			case http.MethodHead:
-				callMethodHandler(w, r, methodHandlers.HEAD)
-			case http.MethodOptions:
-				callMethodHandler(w, r, methodHandlers.OPTIONS)
-			default:
-				Respond.JSON(w, Respond.SError("Method not supported"),
-					http.StatusMethodNotAllowed)
-			}
-		}))
 }
 
 func callMethodHandler(w http.ResponseWriter, r *http.Request, handler func(http.ResponseWriter, *http.Request)) {
